5961f19ba7467babd6583fee8adba7d9cf2e696a
anuragrajanp <anuragrajanp@gmail.com>
2022-12-10 19:21:25 +0530

    Pell Series in C
    Not complete

---
+++ Pell Series/pell_series.c
@@ -0,0 +1,30 @@
+// C program to print pell series.
+
+//CONTRIBUTE TO THE CODE
+
+/*
+
+#include <stdlib.h>
+
+int pell(int num)
+{
+    if (num <= 2)
+        return num;
+    else
+        return (2 * pell(num - 1) + pell(num - 2));
+}
+
+void main()
+{
+    int num;
+    printf("Enter number/limit: ");
+    scanf("&d", num);
+    printf("Pell number at position " << num << " is: " << pell(num);
+    printf("\nPell series till position " << num << " is: ";
+    for (int i = 0; i <= num; i++)
+    {
+        printf(pell(i) << "  ";
+    }
+}
+
+*/
